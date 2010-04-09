b5a77ea4a6068c4536bfbcc03ecd535a123acf8a
Piers Warmers <hello@pierswarmers.com>
2010-04-09 16:44:16 +1000

    Changed helper call to generic method.

--- modules/gnBlogPage/templates/_meta_data.php
+++ modules/gnBlogPage/templates/_meta_data.php
@@ -1,6 +1,6 @@
 <?php use_helper('gnSocialNetworking') ?>
 <dl class="gn-meta-data">
-  <dd><?php echo add_this_badge(array('url' => url_for('gn_blog_page_show',$gn_blog_page, true), 'title' => $gn_blog_page->getTitle())) ?></dd>
+  <dd><?php echo get_social_networking_badge(array('url' => url_for('gn_blog_page_show',$gn_blog_page, true), 'title' => $gn_blog_page->getTitle())) ?></dd>
   <dt><?php echo __('Created') ?>:</dt>
   <dd><?php echo time_ago_in_words_abbr($gn_blog_page->getCreatedAt(), $sf_user->getCulture()) ?></dd>
   <dt><?php echo __('Updated') ?>:</dt>
