aeea80a0ca0ed123d54fd5980d5cbcb7695a8af2
Piers Warmers <hello@pierswarmers.com>
2010-05-28 15:36:09 +1000

    Added the social_media include.

--- modules/rtBlogPage/templates/_blog_page.php
+++ modules/rtBlogPage/templates/_blog_page.php
@@ -1,6 +1,8 @@
+<?php use_helper('rtSocialNetworking') ?>
 <div class="rt-page-content clearfix">
   <?php echo markdown_to_html($rt_blog_page->getContent(), $rt_blog_page); ?>
   <dl class="rt-meta-data">
+    <dd><?php echo get_social_networking_badge(array('url' => url_for('rt_blog_page_show',$rt_blog_page, true), 'title' => $rt_blog_page->getTitle())) ?></dd>
     <dt><?php echo __('Created') ?>:</dt>
     <dd><?php echo time_ago_in_words_abbr($rt_blog_page->getCreatedAt(), $sf_user->getCulture()) ?></dd>
     <dt><?php echo __('Updated') ?>:</dt>
