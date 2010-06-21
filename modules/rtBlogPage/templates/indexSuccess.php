a184cce5584a84472bdb3ed33289b3074b3d4585
Piers Warmers <hello@pierswarmers.com>
2010-06-22 09:49:25 +1000

    Changed paginaition on index to use shared partial.

--- modules/rtBlogPage/templates/indexSuccess.php
+++ modules/rtBlogPage/templates/indexSuccess.php
@@ -1,7 +1,7 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate', 'rtSite') ?>
 
 <?php $heading_tag = sfConfig::has('app_rt_blog_title') ? 'h2' : 'h1' ?>
-<div class="rt-blog-page-index">
+<div >
   <?php if(sfConfig::has('app_rt_blog_title')): ?>
   <h1><?php echo __(sfConfig::get('app_rt_blog_title', 'Latest News')) ?></h1>
   <?php endif; ?>
@@ -22,33 +22,6 @@
   <?php else: ?>
   <p class="notice"><?php echo __('No posts available yet, please visit again later.') ?></p>
   <?php endif; ?>
-
-  <?php if ($pager->haveToPaginate()): ?>
-    <p class="rt-pagination">
-      <span class="back">
-        <a href="<?php echo url_for('@rt_blog_page_pagination?page=1') ?>" class="first"><?php echo __('First') ?></a>
-        <a href="<?php echo url_for('@rt_blog_page_pagination?page='.$pager->getPreviousPage()) ?>" class="previous"><?php echo __('Previous') ?></a>
-      </span>
-      <span class="pages">
-      <?php foreach ($pager->getLinks() as $page): ?>
-        <?php if ($page == $pager->getPage()): ?>
-          <?php echo $page ?>
-        <?php else: ?>
-          <a href="<?php echo url_for('@rt_blog_page_pagination?page='.$page) ?>"><?php echo $page ?></a>
-        <?php endif; ?>
-      <?php endforeach; ?>
-      </span>
-      <span class="forward">
-      <a href="<?php echo url_for('@rt_blog_page_pagination?page='.$pager->getNextPage()) ?>" class="next"><?php echo __('Next') ?></a>
-      <a href="<?php echo url_for('@rt_blog_page_pagination?page='.$pager->getLastPage()) ?>" class="last"><?php echo __('Last') ?></a>
-      </span>
-    </p>
-  <?php endif; ?>
-
-  <p class="rt-pagination-desc">
-    <strong><?php echo count($pager) ?></strong> <?php echo __('posts in this blog') ?>
-    <?php if ($pager->haveToPaginate()): ?>
-      - <?php echo __('viewing page') ?> <strong><?php echo $pager->getPage() ?> <?php echo __('of') ?> <?php echo $pager->getLastPage() ?></strong>
-    <?php endif; ?>
-  </p>
 </div>
+
+<?php include_partial('rtAdmin/pagination_public', array('pager' => $pager)); ?>
