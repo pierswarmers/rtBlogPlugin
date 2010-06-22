1fb01edd7cded6b84df9cef647bddac469ade80b
Torsten Hirsch <torsten@wranglers.com.au>
2010-06-22 14:31:31 +1000

    add naming convention to classes.

--- modules/rtBlogPage/templates/_blog_page.php
+++ modules/rtBlogPage/templates/_blog_page.php
@@ -1,13 +1,2 @@
 <?php use_helper('rtSocialNetworking') ?>
-<div class="rt-page-content clearfix">
-  <?php echo markdown_to_html($rt_blog_page->getContent(), $rt_blog_page); ?>
-  <dl class="rt-meta-data">
-    <dd><?php echo get_social_networking_badge(array('url' => url_for('rt_blog_page_show',$rt_blog_page, true), 'title' => $rt_blog_page->getTitle())) ?></dd>
-    <dt><?php echo __('Created') ?>:</dt>
-    <dd><?php echo time_ago_in_words_abbr($rt_blog_page->getCreatedAt(), $sf_user->getCulture()) ?></dd>
-    <dt><?php echo __('Updated') ?>:</dt>
-    <dd><?php echo time_ago_in_words_abbr($rt_blog_page->getUpdatedAt(), $sf_user->getCulture()) ?></dd>
-    <dt><?php echo __('Version') ?>:</dt>
-    <dd><?php echo $rt_blog_page->version ?></dd>
-  </dl>
-</div>
+<?php echo markdown_to_html($rt_blog_page->getContent(), $rt_blog_page); ?>
--- modules/rtBlogPage/templates/indexSuccess.php
+++ modules/rtBlogPage/templates/indexSuccess.php
@@ -1,27 +1,26 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate', 'rtSite') ?>
-
 <?php $heading_tag = sfConfig::has('app_rt_blog_title') ? 'h2' : 'h1' ?>
-<div >
-  <?php if(sfConfig::has('app_rt_blog_title')): ?>
-  <h1><?php echo __(sfConfig::get('app_rt_blog_title', 'Latest News')) ?></h1>
-  <?php endif; ?>
-  <?php if(count($pager->getResults()) > 0): ?>
-  <div class="rt-blog-page-index-list">
-    <?php foreach ($pager->getResults() as $rt_blog_page): ?>
-    <div class="rt-blog-page-index-item rt-admin-edit-tools-panel">
-      <?php echo link_to(__('Edit'), 'rtBlogPageAdmin/edit?id='.$rt_blog_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
-      <<?php echo $heading_tag ?>><?php echo link_to($rt_blog_page->getTitle(), 'rt_blog_page_show',$rt_blog_page) ?></<?php echo $heading_tag ?>>
-      <?php if(sfConfig::get('app_rt_blog_index_mode', 'full') === 'full'): ?>
-      <?php include_partial('blog_page', array('rt_blog_page' => $rt_blog_page, 'sf_cache_key' => $rt_blog_page->getId())) ?>
-      <?php elseif(sfConfig::get('app_rt_blog_index_mode') === 'description'): ?>
-      <p><?php echo $rt_blog_page->getDescription(); ?></p>
-      <?php endif; ?>
-    </div>
-    <?php endforeach; ?>
-  </div>
-  <?php else: ?>
-  <p class="notice"><?php echo __('No posts available yet, please visit again later.') ?></p>
-  <?php endif; ?>
-</div>
 
+<?php if(sfConfig::has('app_rt_blog_title')): ?>
+  <h1><?php echo __(sfConfig::get('app_rt_blog_title', 'Latest News')) ?></h1>
+<?php endif; ?>
+<?php if(count($pager->getResults()) > 0): ?>
+  <div class="rt-container rt-conllection">
+    <?php $i = 1; foreach ($pager->getResults() as $rt_blog_page): ?>
+      <div class="rt-list-item rt-list-item-<?php echo $i; ?> rt-admin-edit-tools-panel">
+        <?php echo link_to(__('Edit'), 'rtBlogPageAdmin/edit?id='.$rt_blog_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
+        <<?php echo $heading_tag ?>><?php echo link_to($rt_blog_page->getTitle(), 'rt_blog_page_show',$rt_blog_page) ?></<?php echo $heading_tag ?>>
+        <?php if(sfConfig::get('app_rt_blog_index_mode', 'full') === 'full'): ?>
+          <div class="rt-container">
+            <?php include_partial('blog_page', array('rt_blog_page' => $rt_blog_page, 'sf_cache_key' => $rt_blog_page->getId())) ?>
+          </div>
+        <?php elseif(sfConfig::get('app_rt_blog_index_mode') === 'description'): ?>
+          <p><?php echo $rt_blog_page->getDescription(); ?></p>
+        <?php endif; ?>
+      </div>
+    <?php $i++; endforeach; ?>
+  </div>
+<?php else: ?>
+  <p class="notice"><?php echo __('No posts available yet, please visit again later.') ?></p>
+<?php endif; ?>
 <?php include_partial('rtAdmin/pagination_public', array('pager' => $pager)); ?>
