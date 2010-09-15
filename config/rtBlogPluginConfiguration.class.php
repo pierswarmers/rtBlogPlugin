e455ded0435796e78b2da69f3038f6cb997813b8
Konny <konny@wranglers.com.au>
2010-09-15 16:15:53 +1000

    Added new routes

--- config/rtBlogPluginConfiguration.class.php
+++ config/rtBlogPluginConfiguration.class.php
@@ -25,25 +25,10 @@ class rtBlogPluginConfiguration extends sfPluginConfiguration
   {
     $routing = $event->getSubject();
 
+    // Page
     $routing->prependRoute(
       'rt_blog_page_index',
       new sfRoute('/blog',array('module' => 'rtBlogPage', 'action' => 'index'))
-    );
-
-    $routing->prependRoute(
-      'rt_blog_page_feed',
-      new sfRoute('/blog/feed.:format',
-              array('module' => 'rtBlogPage', 'action' => 'feed', 'format' => 'atom1'),
-              array('format' => 'atom1|rss10|rss091|rss201|rss|atom')));
-
-    $routing->prependRoute(
-      'rt_blog_page_show',
-      new sfDoctrineRoute(
-        '/blog/:id/:slug',
-          array('module' => 'rtBlogPage', 'action' => 'show'),
-          array('id' => '\d+', 'sf_method' => array('get')),
-          array('model' => 'rtBlogPage', 'type' => 'object')
-      )
     );
 
     $routing->prependRoute(
@@ -51,14 +36,57 @@ class rtBlogPluginConfiguration extends sfPluginConfiguration
       new sfRoute('/blog/page/:page',array('module' => 'rtBlogPage', 'action' => 'index'))
     );
 
+    // Year only
     $routing->prependRoute(
-            'rt_blog_page_undelete',
-            new sfDoctrineRoute(
-            '/blog/undelete/:id',
-            array('module' => 'rtBlogPage', 'action' => 'undelete'),
-            array('id' => '\d+', 'sf_method' => array('get')),
-            array('model' => 'rtBlogPage', 'type' => 'object')
-            )
+      'rt_blog_page_y',
+      new sfRoute('/blog/:year',array('module' => 'rtBlogPage', 'action' => 'index'))
+    );
+
+    $routing->prependRoute(
+      'rt_blog_page_y_pagination',
+      new sfRoute('/blog/:year/page/:page',array('module' => 'rtBlogPage', 'action' => 'index'))
+    );
+
+    // Year, month only
+    $routing->prependRoute(
+      'rt_blog_page_ym',
+      new sfRoute('/blog/:year/:month',array('module' => 'rtBlogPage', 'action' => 'index'))
+    );
+
+    $routing->prependRoute(
+      'rt_blog_page_ym_pagination',
+      new sfRoute('/blog/:year/:month/page/:page',array('module' => 'rtBlogPage', 'action' => 'index'))
+    );
+
+    // Year, month and day
+    $routing->prependRoute(
+      'rt_blog_page_ymd',
+      new sfRoute('/blog/:year/:month/:day',array('module' => 'rtBlogPage', 'action' => 'index'))
+    );
+
+    $routing->prependRoute(
+      'rt_blog_page_ymd_pagination',
+      new sfRoute('/blog/:year/:month/:day/page/:page',array('module' => 'rtBlogPage', 'action' => 'index'))
+    );
+
+    // Feeds
+    $routing->prependRoute(
+      'rt_blog_page_feed',
+      new sfRoute('/blog/feed.:format',
+              array('module' => 'rtBlogPage', 'action' => 'feed', 'format' => 'atom1'),
+              array('format' => 'atom1|rss10|rss091|rss201|rss|atom')
+      )
+    );
+
+    // Show
+    $routing->prependRoute(
+      'rt_blog_page_show',
+      new sfDoctrineRoute(
+        '/blog/:year/:month/:day/:slug',
+          array('module' => 'rtBlogPage', 'action' => 'show'),
+          array('id' => '\d+', 'sf_method' => array('get')),
+          array('model' => 'rtBlogPage', 'type' => 'object')
+      )
     );
   }
 }
