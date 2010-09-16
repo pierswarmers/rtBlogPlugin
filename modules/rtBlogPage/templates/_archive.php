830664ddc2fb2dfdc903506714c92f51b224d211
Konny <konny@wranglers.com.au>
2010-09-16 10:46:43 +1000

    Added blog archive partial

---
+++ modules/rtBlogPage/templates/_archive.php
@@ -0,0 +1,16 @@
+<?php
+  $options = isset($options) ? $options->getRawValue() : array();
+  $year_header = '';
+?>
+<?php if($rt_blog_posts): ?>
+<dl>
+  <?php $i=0; foreach($rt_blog_posts as $rt_blog_post): ?>
+    <?php $month_name = date("F", mktime(0, 0, 0, $rt_blog_post['month'], 1, $rt_blog_post['year'])); ?>
+    <?php if($year_header != $rt_blog_post['year']): ?>
+      <dt><?php echo $rt_blog_post['year'] ?></dt>
+    <?php endif; ?>
+    <dd><?php echo link_to($month_name, url_for('rt_blog_page_ym',array('year' => $rt_blog_post['year'],'month' => $rt_blog_post['month']))) ?> (<?php echo $rt_blog_post['count'] ?>)</dd>
+    <?php $year_header = $rt_blog_post['year'] ?>
+  <?php $i++; endforeach; ?>
+</dl>
+<?php endif; ?>
