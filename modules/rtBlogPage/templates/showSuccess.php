fac9b40086fdc87cd024dfac34f282c068597a5d
Piers Warmers <hello@pierswarmers.com>
2010-09-17 14:04:11 +1000

    Refined the display logic, include date in title.

--- modules/rtBlogPage/templates/showSuccess.php
+++ modules/rtBlogPage/templates/showSuccess.php
@@ -2,6 +2,9 @@
 
 <div class="rt-blog-page rt-show rt-primary-container rt-admin-edit-tools-panel">
   <?php echo link_to(__('Edit'), 'rtBlogPageAdmin/edit?id='.$rt_blog_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
-  <h1><?php echo $rt_blog_page->getTitle() ?></h1>
+  <h1>
+    <span class="date"><?php echo format_date($rt_blog_page->getPublishedFrom(), 'D', $sf_user->getCulture()) ?></span> 
+    <?php echo $rt_blog_page->getTitle() ?>
+  </h1>
   <?php include_partial('blog_page', array('rt_blog_page' => $rt_blog_page, 'sf_cache_key' => $rt_blog_page->getId())) ?>
 </div>
