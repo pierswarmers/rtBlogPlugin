e1558a7e328e6a66e301483832348d17ff1e8a6e
Konny <konny@wranglers.com.au>
2011-07-11 14:56:27 +1000

    Added rating to comments

--- modules/rtBlogPage/templates/showSuccess.php
+++ modules/rtBlogPage/templates/showSuccess.php
@@ -11,5 +11,5 @@ slot('rt-title', sprintf('<span>%s</span>%s', format_date($rt_blog_page->getPubl
 <?php include_partial('blog_page', array('rt_blog_page' => $rt_blog_page, 'sf_cache_key' => $rt_blog_page->getId())) ?>
 
 <?php if(in_array($rt_blog_page->getCommentStatus(), array('open', 'user'))): ?>
-  <?php include_component('rtComment', 'panel', array('model' => 'rtBlogPage', 'model_id' => $rt_blog_page->getId(), 'title' => $rt_blog_page->getTitle())) ?>
+  <?php include_component('rtComment', 'panel', array('model' => 'rtBlogPage', 'model_id' => $rt_blog_page->getId(), 'title' => $rt_blog_page->getTitle(), 'rating_enabled' => true)) ?>
 <?php endif; ?>
