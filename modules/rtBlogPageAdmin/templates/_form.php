e5498b1e4e2550b3c6b5d1df93f671b36ca72112
Piers Warmers <hello@pierswarmers.com>
2013-03-25 11:49:10 +1100

    Added mode.

--- modules/rtBlogPageAdmin/templates/_form.php
+++ modules/rtBlogPageAdmin/templates/_form.php
@@ -14,78 +14,79 @@
 <?php include_partial('rtAdmin/flashes') ?>
 
 <form id ="rtAdminForm" action="<?php echo url_for('rtBlogPageAdmin/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-<?php echo $form->renderHiddenFields(false) ?>
-<?php if (!$form->getObject()->isNew()): ?>
-<input type="hidden" name="sf_method" value="put" />
-<?php endif; ?>
-<input type="hidden" name="rt_post_save_action" value="edit" />
-  <table>
-    <tbody>
-      <?php echo $form->renderGlobalErrors() ?>
-      <?php echo render_form_row($form['title']); ?>
-      <?php echo render_form_row($form['content']); ?>
-    </tbody>
-  </table>
-
-  <div class="rt-admin-toggle-panel">
-    <h2><?php echo __('Publish Status') ?></h2>
-    <table class="rt-admin-toggle-panel-content">
-      <tbody>
-        <?php echo render_form_row($form['published']); ?>
-        <?php echo render_form_row($form['published_from']); ?>
-        <?php echo render_form_row($form['published_to']); ?>
-      </tbody>
+    <?php echo $form->renderHiddenFields(false) ?>
+    <?php if (!$form->getObject()->isNew()): ?>
+        <input type="hidden" name="sf_method" value="put" />
+    <?php endif; ?>
+    <input type="hidden" name="rt_post_save_action" value="edit" />
+    <table>
+        <tbody>
+        <?php echo $form->renderGlobalErrors() ?>
+        <?php echo render_form_row($form['title']); ?>
+        <?php echo render_form_row($form['content']); ?>
+        <?php echo render_form_row($form['mode']); ?>
+        </tbody>
     </table>
-  </div>
 
-  <div class="rt-admin-toggle-panel">
-    <h2><?php echo __('Metadata and SEO') ?></h2>
-    <table class="rt-admin-toggle-panel-content" id="rtSortableCategories">
-      <tbody>      
-        <?php echo render_form_row($form['description']); ?>
-        <?php echo render_form_row($form['tags']); ?>
-        <?php if(isset ($form['rt_categories_list'])): ?>
-          <?php echo render_form_row($form['rt_categories_list']); ?>        
-        <?php endif; ?>
-      </tbody>
-    </table>
-    <script type="text/javascript">
-    $(function() {
-      $("#rtSortableCategories ul.checkbox_list").first().sortable({
-        revert: true
-      });
-    });
-    </script>
-  </div>
+    <div class="rt-admin-toggle-panel">
+        <h2><?php echo __('Publish Status') ?></h2>
+        <table class="rt-admin-toggle-panel-content">
+            <tbody>
+            <?php echo render_form_row($form['published']); ?>
+            <?php echo render_form_row($form['published_from']); ?>
+            <?php echo render_form_row($form['published_to']); ?>
+            </tbody>
+        </table>
+    </div>
 
-  <div class="rt-admin-toggle-panel">
-    <h2><?php echo __('Comments') ?></h2>
-    <table class="rt-admin-toggle-panel-content">
-      <tbody>
-        <?php echo render_form_row($form['comment_status']); ?>
-      </tbody>
-    </table>
-  </div>
+    <div class="rt-admin-toggle-panel">
+        <h2><?php echo __('Metadata and SEO') ?></h2>
+        <table class="rt-admin-toggle-panel-content" id="rtSortableCategories">
+            <tbody>
+            <?php echo render_form_row($form['description']); ?>
+            <?php echo render_form_row($form['tags']); ?>
+            <?php if(isset ($form['rt_categories_list'])): ?>
+                <?php echo render_form_row($form['rt_categories_list']); ?>
+            <?php endif; ?>
+            </tbody>
+        </table>
+        <script type="text/javascript">
+            $(function() {
+                $("#rtSortableCategories ul.checkbox_list").first().sortable({
+                    revert: true
+                });
+            });
+        </script>
+    </div>
 
-  <div class="rt-admin-toggle-panel">
-    <h2><?php echo __('Author Details') ?></h2>
-    <table class="rt-admin-toggle-panel-content">
-      <tbody>
-        <?php echo render_form_row($form['author_name']); ?>
-        <?php echo render_form_row($form['author_email']); ?>        
-      </tbody>
-    </table>
-  </div>
+    <div class="rt-admin-toggle-panel">
+        <h2><?php echo __('Comments') ?></h2>
+        <table class="rt-admin-toggle-panel-content">
+            <tbody>
+            <?php echo render_form_row($form['comment_status']); ?>
+            </tbody>
+        </table>
+    </div>
 
-  <div class="rt-admin-toggle-panel">
-    <h2><?php echo __('Location and Referencing') ?></h2>
-    <table class="rt-admin-toggle-panel-content">
-      <tbody>
-        <?php echo render_form_row($form['slug']); ?>
-      <?php if(isset($form['site_id'])): ?>
-        <?php echo render_form_row($form['site_id']); ?>
-      <?php endif; ?>
-      </tbody>
-    </table>
-  </div>
+    <div class="rt-admin-toggle-panel">
+        <h2><?php echo __('Author Details') ?></h2>
+        <table class="rt-admin-toggle-panel-content">
+            <tbody>
+            <?php echo render_form_row($form['author_name']); ?>
+            <?php echo render_form_row($form['author_email']); ?>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="rt-admin-toggle-panel">
+        <h2><?php echo __('Location and Referencing') ?></h2>
+        <table class="rt-admin-toggle-panel-content">
+            <tbody>
+            <?php echo render_form_row($form['slug']); ?>
+            <?php if(isset($form['site_id'])): ?>
+                <?php echo render_form_row($form['site_id']); ?>
+            <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
 </form>
